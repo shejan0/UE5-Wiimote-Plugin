6263f9177d1ced4549db312268c993c91c27bcea
Ferdom-M <53622090+Ferdom-M@users.noreply.github.com>
2024-05-27 23:46:57 +0200

    Added rumble, added ir and motion support. Wiimote now uses its own gamepad key names. Adding dll to Binaries folder is no longer necessary.

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # UE4-Wiimote-Plugin
 
 place the "Wiimote" folder under UnrealEngine/Engine/Plugins/Experimental/, and regenerate&build the project.
-copy the "wiiuse.dll" into UnrealEngine/Engine/Binaries/Win64/
 
 # Connect Guide: 
 * RVL-CNT-01: http://www.dolphin-emulator.com/connect-wiimote.html
--- Wiimote/Source/Wiimote/Wiimote.Build.cs
+++ Wiimote/Source/Wiimote/Wiimote.Build.cs
@@ -8,38 +8,42 @@ namespace UnrealBuildTool.Rules
 	{
         private string ModulePath
         {
-            get { return Path.GetDirectoryName(RulesCompiler.GetModuleFilename(this.GetType().Name)); }
+            get { return ModuleDirectory; }
         }
 		private string ThirdPartyPath
 		{
 			get { return Path.GetFullPath(Path.Combine(ModulePath, "../../ThirdParty/")); }
 		}
 
-		public Wiimote(TargetInfo Target)
+		public Wiimote(ReadOnlyTargetRules Target) : base(Target)
 		{
 
             PublicIncludePaths.AddRange(
                 new string[] {
-                    "Wiimote/Public"
+					PluginDirectory + "/Source/Wiimote/Public"
 					// ... add public include paths required here ...
 				}
                 );
 
 			PrivateIncludePaths.AddRange(
 				new string[] {
-					"Wiimote/Private",
+					PluginDirectory + "/Source/Wiimote/Private",
 					// ... add other private include paths required here ...
 				}
 				);
 
             PublicDependencyModuleNames.AddRange(
                 new string[]
-				{
-					"Core",
+                {
+                    "Core",
 					"CoreUObject",
 					"Engine",
-					"InputDevice",
+                    "Projects",
+                    "ApplicationCore", // Needed for UE 4.20
 					"InputCore",
+                    "InputDevice",
+					"SlateCore",
+                    "Slate"
 					// ... add other public dependencies that you statically link with here ...
 				}
                 );
@@ -50,7 +54,12 @@ namespace UnrealBuildTool.Rules
 					"Core",
 					"CoreUObject",
 					"Engine",
+                    "Projects",
+                    "ApplicationCore", // Needed for UE 4.20
+					"InputCore",
 					"InputDevice",
+                    "SlateCore",
+                    "Slate"
 					// ... add private dependencies that you statically link with here ...
 				}
 				);
@@ -62,14 +71,15 @@ namespace UnrealBuildTool.Rules
 				PrivateIncludePaths.Add(Path.Combine(ThirdPartyPath, "WiiUse/inc"));
 			}
 
-	        if (UEBuildConfiguration.bBuildEditor == true)
+	       /* if (UEBuildConfiguration.bBuildEditor == true)
             {
                 //@TODO: Needed for the triangulation code used for sprites (but only in editor mode)
                 //@TOOD: Try to move the code dependent on the triangulation code to the editor-only module
               //  PrivateDependencyModuleNames.AddRange(new string [] {"UnrealEd","ContentBrowser"});
-            }
-
+            }*/
             PublicDelayLoadDLLs.Add("wiiuse.dll");
+            RuntimeDependencies.Add(ThirdPartyPath  + "/WiiUse/lib/Win64/wiiuse.dll");
+
         }
-	}
+    }
 }
